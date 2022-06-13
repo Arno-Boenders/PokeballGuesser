6d9dc8e1c98979d6fb6dc029dae66f3d99c23aa1
arnoboenders <83141630+Arno-Boenders@users.noreply.github.com>
2022-06-13 19:43:21 +0200

    cleanup

--- src/js/tensorflow.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById("bg");
-
-// Load the model.
-cocoSsd.load().then((model) => {
-  // detect objects in the image.
-  model.detect(canvas).then((predictions) => {
-    console.log("Predictions: ", predictions);
-  });
-});
